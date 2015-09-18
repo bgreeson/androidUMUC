8a8a6fe5275c0d04a1cac6441aeb0146d7b7bc26
z-ccalkins <z.ccalkins@gmail.com>
2015-09-18 19:51:25 -0400

    Simple File upload test
    This is the code I used to test the file upload to the app engine

---
+++ web_service/web_prototype/SoundWave_Test002/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>SoundWave</title>
+<link rel="stylesheet" type="text/css" href="http://androidsoundappproject.appspot.com/inc/topcoat-0.8.0/css/topcoat-mobile-dark.css">
+<link rel="stylesheet" type="text/css" href="http://androidsoundappproject.appspot.com/inc/css/main.css">
+
+</head>
+<body>
+<?php
+    echo 'Hello world! Welcome to Soundwave';
+?>
+<br><br>
+<form method="get" action="/upload">
+    <button type="submit">Uploader</button>
+</form>
+
+</body>
+</html>
+ 
+
